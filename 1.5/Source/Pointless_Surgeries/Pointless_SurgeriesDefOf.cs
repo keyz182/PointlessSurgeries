201f88e0b299e741aa1a4824856a02596018bb94
Kieran David Evans <keyz182@gmail.com>
2024-06-24 10:10:04 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/Pointless_Surgeries/Pointless_SurgeriesDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Pointless_Surgeries;
+
+[DefOf]
+public static class Pointless_SurgeriesDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static Pointless_SurgeriesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(Pointless_SurgeriesDefOf));
+}
---
+++ 1.5/Source/Pointless_Surgeries/Pointless_SurgeriesMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Pointless_Surgeries;
+
+public class Pointless_SurgeriesMod : Mod
+{
+    public static Settings settings;
+
+    public Pointless_SurgeriesMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Pointless Surgeries");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.Pointless_Surgeries.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Pointless Surgeries_SettingsCategory".Translate();
+    }
+}
