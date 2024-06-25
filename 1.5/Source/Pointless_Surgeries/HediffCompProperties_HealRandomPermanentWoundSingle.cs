dcfe1ffa5c091622da55310b557c89484cb7f226
Kieran David Evans <keyz182@gmail.com>
2024-06-25 10:57:35 +0100

    Update textures, random heal chance to thoughts and prayers

---
+++ 1.5/Source/Pointless_Surgeries/HediffCompProperties_HealRandomPermanentWoundSingle.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace Pointless_Surgeries;
+
+public class HediffCompProperties_HealRandomPermanentWoundSingle : HediffCompProperties
+{
+    public float chance;
+    protected HediffCompProperties_HealRandomPermanentWoundSingle() => this.compClass = typeof (HediffComp_HealRandomPermanentWoundSingle);
+}
---
+++ 1.5/Source/Pointless_Surgeries/HediffComp_HealRandomPermanentWoundSingle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Pointless_Surgeries;
+
+public class HediffComp_HealRandomPermanentWoundSingle : HediffComp
+{
+    private HediffCompProperties_HealRandomPermanentWoundSingle Props => (HediffCompProperties_HealRandomPermanentWoundSingle) this.props;
+    
+    public override void CompPostPostAdd(DamageInfo? dinfo)
+    {
+        if (Rand.Chance(Props.chance))
+            HediffComp_HealPermanentWounds.TryHealRandomPermanentWound(this.Pawn, this.parent.LabelCap);
+    }
+
+    public static void TryHealRandomPermanentWound(Pawn pawn, string cause)
+    {
+        Hediff result;
+        if (!pawn.health.hediffSet.hediffs.Where<Hediff>((Func<Hediff, bool>) (hd => hd.IsPermanent() || hd.def.chronic)).TryRandomElement<Hediff>(out result))
+            return;
+        HealthUtility.Cure(result);
+        if (!PawnUtility.ShouldSendNotificationAbout(pawn))
+            return;
+        Messages.Message((string) "MessagePermanentWoundHealed".Translate((NamedArgument) cause, (NamedArgument) pawn.LabelShort, (NamedArgument) result.Label, pawn.Named("PAWN")), (LookTargets) (Thing) pawn, MessageTypeDefOf.PositiveEvent);
+    }
+    
+}
