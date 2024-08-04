4ea156229efe5b8769bad811498e91d06ea034bb
Kieran David Evans <keyz182@gmail.com>
2024-08-04 12:18:14 +0100

    feat: jumpy pawns when eating a super cucumber

---
+++ 1.5/Source/Pointless_Surgeries/HediffCompProperties_JumpingBean.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Pointless_Surgeries;
+
+public class HediffCompProperties_JumpingBean : HediffCompProperties
+{
+    public IntRange JumpFrequency;
+    
+    public HediffCompProperties_JumpingBean()
+    {
+        this.compClass = typeof (HediffComp_JumpingBean);
+    }
+}
---
+++ 1.5/Source/Pointless_Surgeries/HediffComp_JumpingBean.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Pointless_Surgeries;
+
+public class HediffComp_JumpingBean: HediffComp
+{
+    public int NextJumpTick = -1;
+    
+    public HediffCompProperties_JumpingBean Props
+    {
+        get => (HediffCompProperties_JumpingBean) this.props;
+    }
+    
+    public override void CompPostTick(ref float severityAdjustment)
+    {
+        EffecterDef flightEffecterDef = DefDatabase<EffecterDef>.GetNamed("JumpFlightEffect", false);
+        SoundDef landingSound = DefDatabase<SoundDef>.GetNamed("Longjump_Land", false);
+        
+        if (NextJumpTick <= 0)
+        {
+            Map map = Pawn.Map;
+            
+            bool selected = Find.Selector.IsSelected(Pawn);
+            
+            IntVec3 cell = CellFinder.RandomClosewalkCellNear(Pawn.Position, map, 18);
+            
+            PawnFlyer pawnFlyer = PawnFlyer.MakeFlyer(ThingDefOf.PawnFlyer, Pawn, cell, flightEffecterDef,
+                landingSound);
+            if (pawnFlyer != null)
+                FleckMaker.ThrowDustPuff(Pawn.Position.ToVector3Shifted() + Gen.RandomHorizontalVector(0.5f), map, 2f);
+            GenSpawn.Spawn(pawnFlyer, cell, map, WipeMode.Vanish);
+            if (selected) Find.Selector.Select(Pawn, false, false);
+            NextJumpTick = Props.JumpFrequency.RandomInRange;
+        }
+        else
+        {
+            NextJumpTick--;
+        }
+    }
+}
