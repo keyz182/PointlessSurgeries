f651e5442263cfbee97061da4883bb18248fd678
Kieran David Evans <keyz182@gmail.com>
2024-08-07 18:43:16 +0100

    Fix destiny manifester spawning with raids, add plaque gun

---
+++ 1.5/Source/Pointless_Surgeries/ModExtension_PlaqueBullet.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace Pointless_Surgeries;
+
+public class ModExtension_PlaqueBullet : DefModExtension
+{
+    public float addHediffChance = 0.05f;
+    public HediffDef hediffToAdd;
+}
---
+++ 1.5/Source/Pointless_Surgeries/Projectile_PlaqueBullet.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Pointless_Surgeries;
+
+public class Projectile_PlaqueBullet: Bullet
+{
+    public ModExtension_PlaqueBullet Props => def.GetModExtension<ModExtension_PlaqueBullet>();
+
+    public override void Impact(Thing hitThing, bool blockedByShield = false)
+    {
+        base.Impact(hitThing, blockedByShield);
+        if (Props == null || hitThing is not Pawn hitPawn) return;
+        
+        float rand = Rand.Value;
+        if (!(rand <= Props.addHediffChance))
+        {
+            MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "PS_PlaqueBullet_FailureMote".Translate(Props.addHediffChance), 12f);;
+            return;
+        };
+        
+        Messages.Message("PS_PlaqueBullet_SuccessMessage".Translate(
+            launcher.Label, hitPawn.Label
+        ), MessageTypeDefOf.NeutralEvent);
+        Hediff plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+        float randomSeverity = Rand.Range(0.15f, 0.30f);
+        if (plagueOnPawn != null)
+        {
+            plagueOnPawn.Severity += randomSeverity;
+        }
+        else
+        {
+            Hediff hediff = HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+            hediff.Severity = randomSeverity;
+            hitPawn.health?.AddHediff(hediff);
+        }
+    }
+}
